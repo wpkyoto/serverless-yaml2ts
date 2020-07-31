41c536bb03d49279afe9307da9c10a08c48aa929
Hidetaka Okamoto <info@wp-kyoto.net>
2020-07-31 19:59:24 +0900

    init

---
+++ README.md
@@ -0,0 +1,32 @@
+serverless-yaml2ts
+==================
+
+Simple convertor from serverless.yml to serverless.ts
+
+[![oclif](https://img.shields.io/badge/cli-oclif-brightgreen.svg)](https://oclif.io)
+[![Version](https://img.shields.io/npm/v/serverless-yaml2ts.svg)](https://npmjs.org/package/serverless-yaml2ts)
+[![Downloads/week](https://img.shields.io/npm/dw/serverless-yaml2ts.svg)](https://npmjs.org/package/serverless-yaml2ts)
+[![License](https://img.shields.io/npm/l/serverless-yaml2ts.svg)](https://github.com/npm/serverless-yaml2ts/blob/master/package.json)
+
+<!-- toc -->
+* [Usage](#usage)
+* [Commands](#commands)
+<!-- tocstop -->
+# Usage
+<!-- usage -->
+```sh-session
+$ npm install -g serverless-yaml2ts
+$ sls-yaml2ts COMMAND
+running command...
+$ sls-yaml2ts (-v|--version|version)
+serverless-yaml2ts/0.0.0 darwin-x64 node-v12.9.1
+$ sls-yaml2ts --help [COMMAND]
+USAGE
+  $ sls-yaml2ts COMMAND
+...
+```
+<!-- usagestop -->
+# Commands
+<!-- commands -->
+
+<!-- commandsstop -->
---
+++ src/index.ts
@@ -0,0 +1,71 @@
+import {Command, flags} from '@oclif/command'
+import { join } from 'path'
+import { readFileSync, writeFileSync, read } from 'fs'
+import { safeLoad } from 'js-yaml'
+
+class ServerlessYaml2Ts extends Command {
+  private readonly targetYML = join(__dirname, '../serverless.yml')
+  private readonly targetYAML = join(__dirname, '../serverless.yaml')
+  private readonly targetTS = join(__dirname, '../serverless.ts')
+  static description = 'describe the command here'
+
+  static flags = {
+    // add --version flag to show CLI version
+    version: flags.version({char: 'v'}),
+    help: flags.help({char: 'h'}),
+    // flag with no value (-f, --force)
+    force: flags.boolean({char: 'f'}),
+    encode: flags.string({
+      char: 'E',
+      default: 'utf-8',
+      description: 'file encode'
+    })
+  }
+  loadYAML(encode: string) {
+    try {
+      this.log(`Loading: ${this.targetYAML}`)
+      const file = readFileSync(this.targetYAML, encode)
+      return file
+    } catch (e) {
+      if (e.code !== 'ENOENT') {
+        throw e
+      }
+    }
+    this.log(`serverless.yaml not found.`)
+    this.log(`Loading: ${this.targetYML}`)
+    return readFileSync(this.targetYML, encode)
+  }
+
+  hasTsFile() {
+    const {flags} = this.parse(ServerlessYaml2Ts)
+    if (flags.force) return
+    try {
+      readFileSync(this.targetTS)
+      this.error(`${this.targetTS} is exists.`)
+      this.exit(1)
+    } catch (e) {
+      if (e.code !== 'ENOENT') {
+        throw e
+      }
+      return
+    }    
+  }
+
+  async run() {
+    const {flags} = this.parse(ServerlessYaml2Ts)
+    const encode = flags.encode
+    const file = this.loadYAML(encode)
+    const serverlessJSON = JSON.stringify(safeLoad(file), null, 2)
+    this.hasTsFile()
+    const serverlessTS = `import { Serverless } from 'serverless/aws';
+export const service: Serverless = ${serverlessJSON}
+
+module.exports = service`
+    writeFileSync(join(__dirname, '../serverless.ts'), serverlessTS, {
+      encoding: encode
+    })
+    this.log(`serverless.ts created`)
+  }
+}
+
+export = ServerlessYaml2Ts
